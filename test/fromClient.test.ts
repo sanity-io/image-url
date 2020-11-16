4cdfcad09af2891eaec5076850d9437c2b604966
Bjørge Næss <bjoerge@gmail.com>
2020-11-16 17:43:47 +0100

    Remove @sanity/client as dependency (#28)
    The dependency on @sanity/client is not neccessary since we only expect it to have a certain structure.

--- test/fromClient.test.ts
+++ test/fromClient.test.ts
@@ -1,22 +1,23 @@
-import SanityClient from '@sanity/client'
 import imgUrl from '../src'
 
 describe('init from client', () => {
   test('can get config from client', () => {
-    const client = new SanityClient({projectId: 'abc123', dataset: 'foo', useCdn: true})
+    const client = {
+      clientConfig: {projectId: 'abc123', dataset: 'foo', apiHost: 'https://cdn.sanity.io'},
+    }
     expect(imgUrl(client).image('image-abc123-200x200-png').toString()).toBe(
       'https://cdn.sanity.io/images/abc123/foo/abc123-200x200.png'
     )
   })
 
   test('can get base url from client', () => {
-    const client = new SanityClient({
-      apiHost: 'https://api.sanity.lol',
-      projectId: 'abc123',
-      dataset: 'foo',
-      useCdn: true,
-    })
-
+    const client = {
+      clientConfig: {
+        apiHost: 'https://api.sanity.lol',
+        projectId: 'abc123',
+        dataset: 'foo',
+      },
+    }
     expect(imgUrl(client).image('image-abc123-200x200-png').toString()).toBe(
       'https://cdn.sanity.lol/images/abc123/foo/abc123-200x200.png'
     )
